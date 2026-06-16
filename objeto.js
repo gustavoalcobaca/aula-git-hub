385ac7bedaf3803a63f065060875dbe055c9c086
gustavoalcobaca3 <gustavoalcobaca3@gmail.com>
2026-06-16 18:06:28 +0000

    16/06 atividade objeto

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "gustavo",
+    idade: 20,
+    profissao: "programador",
+    linguagem_de_progração: "javascript"
+}
+
+console.log(pessoa)
+console.log(pessoa.idade)
+console.log(pessoa.linguagem_de_progração)
+console.log(pessoa.nome)
+console.log(pessoa.profissao)
